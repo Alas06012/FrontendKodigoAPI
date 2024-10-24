03d9c5b600e540ee13cca37f9239279eedcc66d1
Alas06012 <diegoalas06@gmail.com>
2024-10-23 23:42:51 -0600

    Update ProtectedRoute
    Se actualizo ProtectedRoute para utilizar el metodo /dashboard de la API

--- src/Auth/ProtectedRoute.jsx
+++ src/Auth/ProtectedRoute.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-    const isAuthenticated = !!localStorage.getItem('token');
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+    const token = localStorage.getItem('token');
 
-    return isAuthenticated ? children : <Navigate to="/login" />;
+    useEffect(() => {
+        const verifyToken = async () => {
+            if (!token) {
+                setIsAuthenticated(false);
+                return;
+            }
+
+            try {
+                const response = await fetch('http://localhost:5000/api/auth/dashboard', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
+
+                if (response.ok) {
+                    setIsAuthenticated(true);
+                } else {
+                    setIsAuthenticated(false);
+                }
+            } catch (error) {
+                console.error('Error en la verificación del token:', error);
+                setIsAuthenticated(false);
+            }
+        };
+
+        verifyToken();
+    }, [token]);
+
+
+    return isAuthenticated ? children : <Link to="/login" />;
 };
 
 export default ProtectedRoute;
